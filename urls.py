c939afd5e213ff668eb608961e9761ffb9c77623
Anton Pirker <anton@ignaz.at>
2019-06-16 11:35:13 +0200

    Try

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.urls import path
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -5,9 +6,13 @@ from rest_framework.response import Response
 from views import about, index
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=255)
+
 class PostViewSet(viewsets.ViewSet):
     def get_queryset(self):
-        return []
+        return Post.objects.all()
 
     def list(self, request):
         resp = Response({"blub": "list"})
