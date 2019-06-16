e624d6a8dec719455c71b7f5e4ceecd79de02b6a
Anton Pirker <anton@ignaz.at>
2019-06-16 11:48:54 +0200

    Removed browsable api

--- settings.py
+++ settings.py
@@ -125,6 +125,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
