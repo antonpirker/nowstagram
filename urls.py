a7104c482a5b0864cf2862e1d095dae4bf42f64f
Anton Pirker <anton@ignaz.at>
2019-06-16 11:41:48 +0200

    Fixed permissions

--- urls.py
+++ urls.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.urls import path
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -6,14 +5,7 @@ from rest_framework.response import Response
 from views import about, index
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.CharField(max_length=255)
-
 class PostViewSet(viewsets.ViewSet):
-    def get_queryset(self):
-        return Post.objects.all()
-
     def list(self, request):
         resp = Response({"blub": "list"})
         return resp
